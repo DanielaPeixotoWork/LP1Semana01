f1420540cdf62d0fe9f82fa8ab65a28c947dc8c5
DanielaPeixotoWork <danielapeixoto.work@gmail.com>
2024-02-23 10:08:08 +0000

    Adding Comments

--- MeuPrograma/Program.cs
+++ MeuPrograma/Program.cs
@@ -2,8 +2,15 @@
 
 namespace MeuPrograma
 {
+/// <summary>
+/// program
+/// </summary>
     class Program
     {
+        /// <summary>
+        /// main
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
