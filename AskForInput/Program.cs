fb5a2ba958b8c31fa5b8c2a3394499d6e50238a3
DanielaPeixotoWork <danielapeixoto.work@gmail.com>
2024-02-23 17:35:17 +0000

    Numero inteiro e numero real

---
+++ AskForInput/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace AskForInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insira um número inteiro: ");
+            
+            string str = Console.ReadLine();
+            int i = int.Parse( str );
+
+            Console.Write("Insira um número real: ");
+            string str2 = Console.ReadLine();
+            float f = float .Parse( str2 );
+
+            
+
+
+ 
+        }
+    }
+}
