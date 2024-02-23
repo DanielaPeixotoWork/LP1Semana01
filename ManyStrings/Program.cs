dd1d4cf947f589c72e8193c1aec73ded3d88f7c4
DanielaPeixotoWork <danielapeixoto.work@gmail.com>
2024-02-23 17:00:06 +0000

    Various Strings

---
+++ ManyStrings/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ManyStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String N1 = "Helloooo!!";
+            String N2 = "This is a star \u2606 !"
+            String N3 = "ol\á e\ a\deu\s"
+        }
+    }
+}
